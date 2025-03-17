f55a764d18b9ef8474849ed3d103fe6c967e1cd0
Rafael Mukhametdzhanov <muhametdzhanov.r@wildberries.work>
2025-03-17 22:38:37 +0300

    ton scan implementation

--- interfaces/ton/client.go
+++ interfaces/ton/client.go
@@ -1,11 +1,16 @@
 package ton
 
+import "net/http"
+
 type Client struct {
 	tonScanURL string
+
+	client *http.Client
 }
 
 func New(tonScanURL string) *Client {
 	return &Client{
 		tonScanURL: tonScanURL,
+		client:     http.DefaultClient,
 	}
 }
--- internal/domain/deposit/deposit.go
+++ internal/domain/deposit/deposit.go
@@ -3,8 +3,14 @@ package deposit
 import "time"
 
 type Deposit struct {
-	Amount     int64
-	TXID       string
-	TelegramID int64
-	TS         time.Time
+	TxLT        int64     `json:"TxLT"`
+	Token       string    `json:"Token"`
+	Wallet      string    `json:"Wallet"`
+	Comment     string    `json:"Comment"`
+	Amount      int64     `json:"Amount"`
+	TXHash      string    `json:"TxHash"`
+	Payload     string    `json:"Payload"`
+	TXTimestamp string    `json:"TxTimestamp"`
+	TXComment   string    `json:"TxComment"`
+	CreatedAt   time.Time `json:"CreatedAt"`
 }
